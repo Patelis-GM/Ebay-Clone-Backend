179a374ac551434c7c8356d49bd483820cc7409d
Giorgos Marios Patelis <pgiorgosmarios@gmail.com>
2022-06-02 05:25:51 +0300

    Commit

--- src/main/java/silkroad/utilities/recommendations/RecommendationMatrix.java
+++ src/main/java/silkroad/utilities/recommendations/RecommendationMatrix.java
@@ -16,10 +16,11 @@ public class RecommendationMatrix {
     private final Logger logger = LoggerFactory.getLogger(RecommendationMatrix.class);
 
     private final double learningRate = 0.001;
-    private final double regularizationPenalty = 0.08;
-    private final int latentFeatures = 2;
+    private final double regularizationPenalty = 0.09
+            ;
+    private final int latentFeatures = 3;
     private final int epochs = 2048;
-    private final int patience = 2;
+    private final int patience = 1;
 
     public RecommendationMatrix() {
         this.recommendationMatrix = null;
